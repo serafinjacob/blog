daa3041853d1669d45b9d90c73587d6c6ade6407
Jacob <serafinj@uoguelph.ca>
2023-12-12 23:50:10 -0500

    added Content containers for about me and projects section - not fully functional yet

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,27 +1,73 @@
-import imagewrapper from "@/styles/Image.module.css";
-import Image from "next/image";
+import ContentContainerLayout from "@/components/ContentContainer/Layout";
+import ContentContainerUI from '/components/ContentContainer/UI/ContentContainer.module.css'
 
 export default function Page() {
     return (
-        <div className="text-left">
-            <h1 className="text-4xl font-bold pt-10">
+        <div className="m-[50px]">
+            <h1 className="text-left text-4xl font-bold pb-3">
                 Education
             </h1>
-            <h2 className="mt-3 text-2xl pt-3 font-bold">
-                University of Guelph
-            </h2>
-            <h3 className="text-l">
-                Bachelor of Computing, Honours
-            </h3>
+            <div className={ContentContainerUI.gridview}>
+                <div>
+                    <ContentContainerLayout {...{
+                        title: 'University of Guelph',
+                        description: 'Bachelor of Computing, Honours',
+                        image: 'https://img.emg-services.net/institutes/institute9925/untitled-design-10-1-1-1.png',
+                        route: '/about',
+                        icon: '',
+                    }}
+                    />
+                </div>
 
-            <h2 className="mt-3 text-2xl pt-3 font-bold whitespace-nowrap">
-                Toronto Metropolitan University
-            </h2>
-            <p className="text-s italic">(formerly Ryerson University)</p>
+                <div>
+                    <ContentContainerLayout {...{
+                        title: 'Toronto Metropolitan University',
+                        description: 'Certificate in Business Communication',
+                        image: 'https://www.viewthevibe.com/wp-content/uploads/2021/08/IEO-IntLand-2220x1100-header.jpg',
+                        route: '/about',
+                        icon: '',
+                    }}
+                    />
+                </div>
+            </div>
 
-            <h3 className="text-l">
-                Certificate in Business Communications
-            </h3>
+            <h1 className="text-left text-4xl font-bold pb-3 pt-3">
+                Work Experience
+            </h1>
+            <div className={ContentContainerUI.gridview}>
+                <div>
+                    <ContentContainerLayout {...{
+                        title: 'Magna International',
+                        description: 'Forklift Operator',
+                        image: 'https://smartcdn.gprod.postmedia.digital/financialpost/wp-content/uploads/2021/04/vw0413magna.jpg',
+                        route: '/about',
+                        icon: '',
+                    }}
+                    />
+                </div>
+
+                <div>
+                    <ContentContainerLayout {...{
+                        title: 'Walmart Canada',
+                        description: 'Fulfillment Associate',
+                        image: 'https://corporate.walmart.com/content/corporate/en_us/about/international/walmart-canada/jcr:content/par/image_2_0.img.jpg/1693432664791.jpg',
+                        route: '/about',
+                        icon: '',
+                    }}
+                    />
+                </div>
+
+                <div>
+                    <ContentContainerLayout {...{
+                        title: 'The Home Depot Canada',
+                        description: 'Receiving Supervisor',
+                        image: 'https://www.shutterstock.com/shutterstock/photos/1175525740/display_1500/stock-photo-toronto-ontario-canada-september-home-depot-canada-corporate-office-building-in-1175525740.jpg',
+                        route: '/about',
+                        icon: '',
+                    }}
+                    />
+                </div>
+            </div>
         </div>
     );
 }
